58a45bf41395ff360c11a6bbe962e67a52ac7a77
Sunday Ishaya <ishayasunday@gmail.com>
2020-11-02 14:23:06 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,86 @@
+# Amazona ECommerce Website
+
+This is a fully-functional e-commerce website exactly like amazon. I built an e-commerce website using MERN stack (MongoDB, ExpressJS, React and Node.JS).
+
+
+## Technologies used
+
+- HTML5 and CSS3: Semantic Elements, CSS Grid, Flexbox
+- React: Components, Props, Events, Hooks, Router, Axios
+- Redux: Store, Reducers, Actions
+- Node & Express: Web API, Body Parser, File Upload, JWT
+- MongoDB: Mongoose, Aggregation
+- Development: ESLint, Babel, Git, Github
+- Payment with Paypal and Stripe
+
+## Run Locally
+
+### 1. Clone repo
+
+```
+$ git clone git@github.com:imizallah/MERN-Stack-E-commerce-System.git
+```
+
+### 2. Setup MongoDB
+
+- Local MongoDB
+  - Install it from [here](https://www.mongodb.com/try/download/community)
+  - Create .env file in root folder
+  - Set MONGODB_URL=mongodb://localhost/imizallahamazon  ***
+- Atlas Cloud MongoDB
+  - Create database at [https://cloud.mongodb.com](https://cloud.mongodb.com)
+  - Create .env file in root folder
+  - Set MONGODB_URL=mongodb+srv://your-db-connection  ***
+
+### 3. Run Backend
+
+```
+$ npm install
+$ npm start
+```
+
+### 4. Run Frontend
+
+```
+# open new terminal
+$ cd frontend
+$ npm install
+$ npm start
+```
+
+### 5. Seed Users and Products
+
+- Run this on chrome: http://localhost:5000/api/users/seed
+- It returns admin email and password
+
+- Run this on chrome: http://localhost:5000/api/products/seed
+- It creates 6 sample products
+
+
+### 6. User Login
+- Run http://localhost:3000/signin
+- Enter admin email and password and click signin
+- User can view all products
+- User can view single products
+- User can Add products to cart
+- User can Checkout products
+- User make create Orders and make payment with `Paypal` or `Stripe` payment gateways
+- User can also track Orders until item is delivered
+
+
+### 7. Admin Login
+
+- Run http://localhost:3000/signin
+- Enter admin email and password and click signin
+- View all Orders
+- View all User
+- Admin can Create Products
+- Mark any order as Delivered
+- Admin can Edit any Prooduct
+- Admin can Delete Products
+
+## Support
+
+- Contact Me: [Sunday, Ishaya](ishayasunday@gmail.com)
+
+## May the source codes be with you::: +-_-+
---
+++ frontend/src/App.js
@@ -0,0 +1,127 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
+import { signout } from './actions/userActions';
+import AdminRoute from './components/AdminRoute';
+import PrivateRoute from './components/PrivateRoute';
+import CartScreen from './screens/CartScreen';
+import HomeScreen from './screens/HomeScreen';
+import OrderHistoryScreen from './screens/OrderHistoryScreen';
+import OrderScreen from './screens/OrderScreen';
+import PaymentMethodScreen from './screens/PaymentMethodScreen';
+import PlaceOrderScreen from './screens/PlaceOrderScreen';
+import ProductListScreen from './screens/ProductListScreen';
+import ProductScreen from './screens/ProductScreen';
+import ProfileScreen from './screens/ProfileScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import ShippingAddressScreen from './screens/ShippingAddressScreen';
+import SigninScreen from './screens/SigninScreen';
+import ProductEditScreen from './screens/ProductEditScreen';
+import OrderListScreen from './screens/OrderListScreen';
+
+function App() {
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+  const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo } = userSignin;
+  const dispatch = useDispatch();
+  const signoutHandler = () => {
+    dispatch(signout());
+  };
+  return (
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <Link className="brand" to="/">
+              amazona
+            </Link>
+          </div>
+          <div>
+            <Link to="/cart">
+              Cart
+              {cartItems.length > 0 && (
+                <span className="badge">{cartItems.length}</span>
+              )}
+            </Link>
+            {userInfo ? (
+              <div className="dropdown">
+                <Link to="#">
+                  {userInfo.name} <i className="fa fa-caret-down"></i>{' '}
+                </Link>
+                <ul className="dropdown-content">
+                  <li>
+                    <Link to="/profile">User Profile</Link>
+                  </li>
+                  <li>
+                    <Link to="/orderhistory">Order History</Link>
+                  </li>
+                  <li>
+                    <Link to="#signout" onClick={signoutHandler}>
+                      Sign Out
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            ) : (
+              <Link to="/signin">Sign In</Link>
+            )}
+            {userInfo && userInfo.isAdmin && (
+              <div className="dropdown">
+                <Link to="#admin">
+                  Admin <i className="fa fa-caret-down"></i>
+                </Link>
+                <ul className="dropdown-content">
+                  <li>
+                    <Link to="/dashboard">Dashboard</Link>
+                  </li>
+                  <li>
+                    <Link to="/productlist">Products</Link>
+                  </li>
+                  <li>
+                    <Link to="/orderlist">Orders</Link>
+                  </li>
+                  <li>
+                    <Link to="/userlist">Users</Link>
+                  </li>
+                </ul>
+              </div>
+            )}
+          </div>
+        </header>
+        <main>
+          <Route path="/cart/:id?" component={CartScreen}></Route>
+          <Route path="/product/:id" component={ProductScreen} exact></Route>
+          <Route
+            path="/product/:id/edit"
+            component={ProductEditScreen}
+            exact
+          ></Route>
+          <Route path="/signin" component={SigninScreen}></Route>
+          <Route path="/register" component={RegisterScreen}></Route>
+          <Route path="/shipping" component={ShippingAddressScreen}></Route>
+          <Route path="/payment" component={PaymentMethodScreen}></Route>
+          <Route path="/placeorder" component={PlaceOrderScreen}></Route>
+          <Route path="/order/:id" component={OrderScreen}></Route>
+          <Route path="/orderhistory" component={OrderHistoryScreen}></Route>
+          <PrivateRoute
+            path="/profile"
+            component={ProfileScreen}
+          ></PrivateRoute>
+          <AdminRoute
+            path="/productlist"
+            component={ProductListScreen}
+          ></AdminRoute>
+          <AdminRoute
+            path="/orderlist"
+            component={OrderListScreen}
+          ></AdminRoute>
+          <Route path="/" component={HomeScreen} exact></Route>
+        </main>
+        <footer className="row center">All right reserved</footer>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
